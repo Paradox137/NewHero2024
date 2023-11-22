a09d4d6a9d218f5a49b4ef416fb53da04ba47d56
Igor X <iverbitskiy321@gmail.com>
2023-11-22 18:01:54 +0300

    4.SaveTrigger

---
+++ HeroNewYear2024/Assets/Scripts/Logic/SaveTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using HeroScripts.Infrastructure.Services;
+using HeroScripts.Infrastructure.Services.SaveLoad;
+using UnityEngine;
+
+namespace HeroScripts.Logic
+{
+	[RequireComponent(typeof(BoxCollider))]
+	public class SaveTrigger : MonoBehaviour
+	{
+		private ISavedLoadService _saveLoadService;
+		private void Awake()
+		{
+			_saveLoadService = AllServices.Container.Single<ISavedLoadService>();
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			_saveLoadService.SaveProgress();
+
+			Debug.Log("Progress Saved.");
+			gameObject.SetActive(false);
+		}
+
+		private void OnDrawGizmos()
+		{
+			BoxCollider _collider = gameObject.GetComponent<BoxCollider>();
+			
+			Gizmos.color = new Color32(30, 200, 30, 130);
+			
+			Gizmos.DrawCube(transform.position + _collider.center, _collider.size);
+		}
+	}
+}
