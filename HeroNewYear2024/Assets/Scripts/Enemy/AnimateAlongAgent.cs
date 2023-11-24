897c8198318aa8b611a428c8652943d15446772f
Igor X <iverbitskiy321@gmail.com>
2023-11-24 13:51:07 +0300

    5.AnimateAlongAgent

---
+++ HeroNewYear2024/Assets/Scripts/Enemy/AnimateAlongAgent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace HeroScripts.Enemy
+{
+	[RequireComponent(typeof(NavMeshAgent))]
+	[RequireComponent(typeof(EnemyAnimator))]
+	public class AnimateAlongAgent : MonoBehaviour
+	{
+		private const float MinimalVelocity = 0.1f;
+		
+		public NavMeshAgent Agent;
+		public EnemyAnimator Animator;
+
+		private void Update()
+		{
+			if (ShouldMove())
+			{
+				Animator.Move(Agent.velocity.magnitude);
+			}
+			else
+			{
+				Animator.StopMoving();
+			}
+		}
+		private bool ShouldMove()
+		{
+			return Agent.velocity.magnitude > MinimalVelocity && Agent.remainingDistance > Agent.radius;
+		}
+	}
+}
