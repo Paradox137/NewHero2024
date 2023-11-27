26c77c306f6ace301e7ce9e3a8aec72ffd5146b0
Igor X <iverbitskiy321@gmail.com>
2023-11-27 12:02:14 +0300

    6.Hitboxes + Overlap

--- HeroNewYear2024/Assets/Scripts/Enemy/Attack.cs
+++ HeroNewYear2024/Assets/Scripts/Enemy/Attack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using HeroScripts.Infrastructure;
 using HeroScripts.Infrastructure.Services;
 using UnityEngine;
@@ -8,17 +9,24 @@ namespace HeroScripts.Enemy
 	[RequireComponent(typeof(EnemyAnimator))]
 	public class Attack : MonoBehaviour
 	{
-		public EnemyAnimator Animator;
-		public float AttackCooldown = 3f;
+		[SerializeField] private EnemyAnimator Animator;
+		[SerializeField] private float AttackCooldown = 3f;
+		[SerializeField] private float RadiusAttackHit = 0.5f;
+		[SerializeField] private float TestDistance = 0.5f;
 		
 		private IGameFactory _factory;
 		private Transform _heroTransform;
 		private float _attackCooldown;
 		private bool _isAttacking;
+		private int _layerMask;
+		private Collider[] _hits = new Collider[1];
 
 		private void Awake()
 		{
 			_factory = AllServices.Container.Single<IGameFactory>();
+
+			_layerMask = 1 << LayerMask.NameToLayer("Player");
+			
 			_factory.HeroCreated += OnHeroCreated;
 		}
 
@@ -32,7 +40,10 @@ namespace HeroScripts.Enemy
 
 		private void OnAttack()
 		{
-			
+			if (Hit(out Collider hit))
+			{
+				
+			}
 		}
 
 		private void OnAttackEnded()
@@ -40,7 +51,19 @@ namespace HeroScripts.Enemy
 			_attackCooldown = AttackCooldown;
 			_isAttacking = true;
 		}
+		
+		private bool Hit(out Collider hit)
+		{
+			Vector3 startHitPoint = StartHitPoint();
+			
+			int hitCount = Physics.OverlapSphereNonAlloc(startHitPoint, RadiusAttackHit, _hits, _layerMask);
 
+			hit = _hits.FirstOrDefault();
+
+			return hitCount > 0;
+		}
+
+		private Vector3 StartHitPoint() => new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z) + transform.forward * TestDistance;
 		private bool CanAttack() => CooldownEnded() && !_isAttacking;
 		private bool CooldownEnded() => _attackCooldown <= 0;
 		private void OnHeroCreated() => _heroTransform = _factory.HeroGameObject.transform;
