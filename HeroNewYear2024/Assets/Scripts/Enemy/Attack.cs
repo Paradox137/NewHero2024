c20cfd201b81dce271109757f85153d67f23e91c
Igor X <iverbitskiy321@gmail.com>
2023-11-27 11:41:14 +0300

    6.AttackAnimation

---
+++ HeroNewYear2024/Assets/Scripts/Enemy/Attack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using HeroScripts.Infrastructure;
+using HeroScripts.Infrastructure.Services;
+using UnityEngine;
+
+namespace HeroScripts.Enemy
+{
+	[RequireComponent(typeof(EnemyAnimator))]
+	public class Attack : MonoBehaviour
+	{
+		public EnemyAnimator Animator;
+		public float AttackCooldown = 3f;
+		
+		private IGameFactory _factory;
+		private Transform _heroTransform;
+		private float _attackCooldown;
+		private bool _isAttacking;
+
+		private void Awake()
+		{
+			_factory = AllServices.Container.Single<IGameFactory>();
+			_factory.HeroCreated += OnHeroCreated;
+		}
+
+		private void Update()
+		{
+			UpdateCooldown();
+
+			if(CanAttack())
+				StartAttack();
+		}
+
+		private void OnAttack()
+		{
+			
+		}
+
+		private void OnAttackEnded()
+		{
+			_attackCooldown = AttackCooldown;
+			_isAttacking = true;
+		}
+
+		private bool CanAttack() => CooldownEnded() && !_isAttacking;
+		private bool CooldownEnded() => _attackCooldown <= 0;
+		private void OnHeroCreated() => _heroTransform = _factory.HeroGameObject.transform;
+		private void UpdateCooldown()
+		{
+			if (!CooldownEnded())
+				_attackCooldown -= Time.deltaTime;
+		}
+
+		private void StartAttack()
+		{
+			transform.LookAt(_heroTransform);
+			
+			Animator.PlayAttack();
+
+			_isAttacking = true;
+		}
+
+	}
+}
