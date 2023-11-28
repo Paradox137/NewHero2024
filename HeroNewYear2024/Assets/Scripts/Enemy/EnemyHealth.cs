24917259df6bbb147ffcda062c7a6e1ebb24f1ba
Igor X <iverbitskiy321@gmail.com>
2023-11-28 19:49:33 +0300

    7.EnemyHealth

---
+++ HeroNewYear2024/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace HeroScripts.Enemy
+{
+	public class EnemyHealth : MonoBehaviour
+	{
+		public EnemyAnimator Animator;
+
+		[SerializeField]
+		private float _current;
+
+		[SerializeField]
+		private float _max;
+
+		public event Action HealthChanged;
+
+		public float Current
+		{
+			get => _current;
+			set => _current = value;
+		}
+
+		public float Max
+		{
+			get => _max;
+			set => _max = value;
+		}
+
+		public void TakeDamage(float damage)
+		{
+			Current -= damage;
+      
+			Animator.PlayHit();
+      
+			HealthChanged?.Invoke();
+		}
+	}
+}
