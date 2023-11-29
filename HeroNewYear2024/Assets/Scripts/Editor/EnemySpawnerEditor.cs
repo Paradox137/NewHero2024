e3958723c9117e880e5bac8629bb9468c8a1d9a0
Igor X <iverbitskiy321@gmail.com>
2023-11-29 15:16:57 +0300

    8.Gizmos

---
+++ HeroNewYear2024/Assets/Scripts/Editor/EnemySpawnerEditor.cs
@@ -0,0 +1,17 @@
+﻿using HeroScripts.Logic;
+using UnityEditor;
+using UnityEngine;
+
+namespace HeroScripts.Editor
+{
+	[CustomEditor(typeof(EnemySpawner))]
+	public class EnemySpawnerEditor : UnityEditor.Editor
+	{
+		[DrawGizmo(GizmoType.Active | GizmoType.Pickable | GizmoType.NonSelected)]
+		public static void RenderCustomGizmo(EnemySpawner spawner, GizmoType gizmoType)
+		{
+			Gizmos.color = Color.red;
+			Gizmos.DrawSphere(spawner.transform.position, 0.5f);
+		}
+	}
+}
