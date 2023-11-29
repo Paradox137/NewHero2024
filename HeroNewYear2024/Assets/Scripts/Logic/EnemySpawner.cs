89d792d5b19808c09a788d86bf74348275f1c0fe
Igor X <iverbitskiy321@gmail.com>
2023-11-29 15:05:01 +0300

    8.InitialSpawner

---
+++ HeroNewYear2024/Assets/Scripts/Logic/EnemySpawner.cs
@@ -0,0 +1,10 @@
+﻿using HeroScripts.StaticData;
+using UnityEngine;
+
+namespace HeroScripts.Logic
+{
+	public class EnemySpawner : MonoBehaviour
+	{
+		public EnemyTypeID EnemyTypeID;
+	}
+}
---
+++ HeroNewYear2024/Assets/Scripts/StaticData/EnemyTypeID.cs
@@ -0,0 +1,8 @@
+﻿namespace HeroScripts.StaticData
+{
+	public enum EnemyTypeID
+	{
+		Lich = 0,
+		Golem = 10,
+	}
+}
