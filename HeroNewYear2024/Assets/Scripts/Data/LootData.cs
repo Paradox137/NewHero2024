a71bd9782a4073342fd486594f35e58a8eb0a809
Igor X <iverbitskiy321@gmail.com>
2023-12-20 17:19:36 +0300

    10. SimpleReactivity

--- HeroNewYear2024/Assets/Scripts/Data/LootData.cs
+++ HeroNewYear2024/Assets/Scripts/Data/LootData.cs
@@ -6,10 +6,13 @@ namespace HeroScripts.Data
 	public class LootData
 	{
 		public int Collected;
+		//todo event
+		public Action Changed;
 
 		public void Collect(Loot loot)
 		{
 			Collected += loot.Value;
+			Changed?.Invoke();
 		}
 	}
 }
---
+++ HeroNewYear2024/Assets/Scripts/UI/LootCounter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using HeroScripts.Data;
+using TMPro;
+using UnityEngine;
+
+namespace HeroScripts.UI
+{
+	public class LootCounter : MonoBehaviour
+	{
+		public TextMeshProUGUI Counter;
+		private WorldData _worldData;
+
+		public void Construct(WorldData worldData)
+		{
+			_worldData = worldData;
+			_worldData.LootData.Changed += UpdateCounter;
+		}
+		
+		private void Start()
+		{
+			UpdateCounter();
+		}
+
+		private void UpdateCounter()
+		{
+			Counter.text = $"{_worldData.LootData.Collected}";
+		}
+	}
+}
